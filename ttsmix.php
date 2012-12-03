0263a3a835bd607e1811872447d1b2e95acc9f0f
World Wide Web <www@server1.atelierfolklore.ca>
2012-12-03 05:35:00 +0000

    Added mix method

--- ttsmix.php
+++ ttsmix.php
@@ -8,12 +8,15 @@ define('SOX_PATH', '/usr/bin/sox');
 
 //Available voices
 $VOICES = array('fr','en');
+$METHODS = array('concatenate', 'sequence', 'mix', 'mix-power', 'merge', 'multiply');
 
 //Parameters
 $text1 = $_REQUEST['text1'];
 $voice1 = isset($_REQUEST['voice1']) && in_array($_REQUEST['voice1'],$VOICES) ? $_REQUEST['voice1']:'en';
 $text2 = $_REQUEST['text2'];
 $voice2 = isset($_REQUEST['voice2']) && in_array($_REQUEST['voice2'],$VOICES) ? $_REQUEST['voice2']:'en';
+
+$method = isset($_REQUEST['method']) && in_array($_REQUEST['method'],$METHODS) ? $_REQUEST['method']:'mix';
 
 $tmpfname1 = tempnam("/tmp","tts").".wav";
 $tmpfname2 = tempnam("/tmp","tts").".wav";
@@ -27,5 +30,5 @@ if(!isset($_REQUEST['debug'])) {
 }
 exec(ESPEAK_PATH.' -v '.escapeshellarg($voice1).' -w '.escapeshellarg($tmpfname1).' '.escapeshellarg($text1));
 exec(ESPEAK_PATH.' -v '.escapeshellarg($voice2).' -w '.escapeshellarg($tmpfname2).' '.escapeshellarg($text2));
-system(SOX_PATH.' -m -v 1 '.escapeshellarg($tmpfname1).' -v 1 '.escapeshellarg($tmpfname2).' -t wav - | '.LAME_PATH.' --quiet - -');
+system(SOX_PATH.' --combine '.escapeshellarg($method).' -v 1 '.escapeshellarg($tmpfname1).' -v 1 '.escapeshellarg($tmpfname2).' -t wav - | '.LAME_PATH.' --quiet - -');
 exit();
