e0b0a0915952f74e447fc8d9ede5325d223397f8
World Wide Web <www@server1.atelierfolklore.ca>
2012-12-03 05:17:13 +0000

    Bugfix with sox

--- ttsmix.php
+++ ttsmix.php
@@ -5,6 +5,7 @@ define('ESPEAK_PATH', '/usr/local/bin/speak');
 define('LAME_PATH', '/usr/local/bin/lame');
 define('SOX_PATH', '/usr/bin/sox');
 
+
 //Available voices
 $VOICES = array('fr','en');
 
@@ -14,8 +15,8 @@ $voice1 = isset($_REQUEST['voice1']) && in_array($_REQUEST['voice1'],$VOICES) ? 
 $text2 = $_REQUEST['text2'];
 $voice2 = isset($_REQUEST['voice2']) && in_array($_REQUEST['voice2'],$VOICES) ? $_REQUEST['voice2']:'en';
 
-$tmpfname1 = tempnam("/tmp","tts");
-$tmpfname2 = tempnam("/tmp","tts");
+$tmpfname1 = tempnam("/tmp","tts").".wav";
+$tmpfname2 = tempnam("/tmp","tts").".wav";
 
 //Output mp3
 if(!isset($_REQUEST['debug'])) {
@@ -24,8 +25,7 @@ if(!isset($_REQUEST['debug'])) {
 	ini_set('display_errors',1);
 	error_reporting(E_ALL);
 }
-if(!isset($_REQUEST['text']) || empty($_REQUEST['text'])) die();
 exec(ESPEAK_PATH.' -v '.escapeshellarg($voice1).' -w '.escapeshellarg($tmpfname1).' '.escapeshellarg($text1));
 exec(ESPEAK_PATH.' -v '.escapeshellarg($voice2).' -w '.escapeshellarg($tmpfname2).' '.escapeshellarg($text2));
-system(SOX_PATH.' -m -v 1 '.escapeshellarg($voice2).' -v 1 '.escapeshellarg($voice2).' - | '.LAME_PATH.' --quiet - -');
+system(SOX_PATH.' -m -v 1 '.escapeshellarg($tmpfname1).' -v 1 '.escapeshellarg($tmpfname2).' -t wav - | '.LAME_PATH.' --quiet - -');
 exit();
