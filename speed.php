8ec827d63f934a0d9be98ea18268be41f49318d4
David Mongeau-Petitpas <dmongeau_petitpas@ecoutez.ca>
2013-06-28 00:41:56 -0400

    Added speed

---
+++ speed.php
@@ -0,0 +1,18 @@
+<?php
+
+
+//Change path depending on your system
+define('SOX_PATH', '/usr/bin/sox');
+
+//Set headers
+if(!isset($_REQUEST['debug']) || $_REQUEST['debug'] != 'true') {
+	header('Content-type: audio/mpeg;');
+} else {
+	ini_set('display_errors',1);
+	error_reporting(E_ALL);
+}
+
+$url = $_REQUEST['url'];
+$speed = isset($_REQUEST['speed']) && (float)$_REQUEST['speed'] > 0 ? (float)$_REQUEST['speed']:10;
+
+system('curl '.escapeshellarg($url).' | '.SOX_PATH.' -t mp3 - -t mp3 - speed '.escapeshellarg($speed));
