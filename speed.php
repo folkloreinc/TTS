6070963cd0b38ade77b13bb54029f3decc384b27
David Mongeau-Petitpas <dmp@atelierfolklore.ca>
2014-08-11 11:05:16 -0400

    Better speed script

--- speed.php
+++ speed.php
@@ -13,8 +13,12 @@ if(!isset($_REQUEST['debug']) || $_REQUEST['debug'] != 'true') {
 	error_reporting(E_ALL);
 }
 
-$url = $_REQUEST['url'];
+$url = isset($_REQUEST['url']) ? $_REQUEST['url']:null;
 $speed = isset($_REQUEST['speed']) && (float)$_REQUEST['speed'] > 0 ? (float)$_REQUEST['speed']:4;
+$reverb = isset($_REQUEST['reverb']) && (integer)$_REQUEST['reverb'] >= 0 ? (integer)$_REQUEST['reverb']:80;
 
-//$tmpPath = tempnam('/tmp','ttsphp_').'.mp3';
-system('curl '.escapeshellarg($url).' | '.SOX_PATH.' -t mp3 - -t wav - speed '.escapeshellarg($speed).' reverb 80 echo 0.8 0.9 1000.0 0.3 1800.0 0.25 | '.LAME_PATH.' -b 72 --quiet - -');
+if(!preg_match('/^https?\:\/\//',$url) || filter_var($url, FILTER_VALIDATE_URL) === FALSE) {
+	die();
+}
+
+system('curl '.escapeshellarg($url).' | '.SOX_PATH.' -t mp3 - -t wav - speed '.escapeshellarg($speed).' reverb '.escapeshellarg($reverb).' echo 0.8 0.9 1000.0 0.3 1800.0 0.25 | '.LAME_PATH.' -b 72 --quiet - -');
