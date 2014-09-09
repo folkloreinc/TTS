8024265cd69bc0fd0f4b04e32299bd2fa7c82940
David Mongeau-Petitpas <dmp@atelierfolklore.ca>
2014-09-09 17:40:22 -0400

    Debug

--- phone.php
+++ phone.php
@@ -24,6 +24,7 @@ $tmpPath = tempnam('/tmp','ttsphonephp_').'.wav';
 
 $command1 = 'curl %s | '.SOX_PATH.' -t mp3 - -t wav %s vol -3dB mcompand "0.005,0.1 -47,-40,-34,-34,-17,-33" 100 "0.003,0.05 -47,-40,-34,-34,-17,-33" 400 "0.000625,0.0125 -47,-40,-34,-34,-15,-33" 1600 "0.0001,0.025 -47,-40,-34,-34,-31,-31,-0,-30" 6400 "0,0.025 -38,-31,-28,-28,-0,-25" vol 15dB highpass 22 highpass 22';
 $command1 = sprintf($command1, $url, $tmpPath);
+die($command1);
 exec($command1);
 
 $command2 = 'sox %s -p synth whitenoise vol %s | sox -m %s - -t wav - | '.LAME_PATH.' -b 72 --quiet - -';
