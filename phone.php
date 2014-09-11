ce57faa6e81b9b64e181d54362eded81f781b0d3
David Mongeau-Petitpas <dmp@atelierfolklore.ca>
2014-09-11 11:45:24 -0400

    Added options to phone effect

--- phone.php
+++ phone.php
@@ -15,6 +15,9 @@ if(!isset($_REQUEST['debug']) || $_REQUEST['debug'] != 'true') {
 
 $url = isset($_REQUEST['url']) ? $_REQUEST['url']:null;
 $noise = '0.0'.(isset($_REQUEST['noise']) && $_REQUEST['noise'] >= 0 ? $_REQUEST['noise']:rand(1,5));
+$pitch = (isset($_REQUEST['pitch']) && (int)$_REQUEST['pitch'] != 0 ? $_REQUEST['pitch']:0;
+$speed = (isset($_REQUEST['speed']) && (int)$_REQUEST['speed'] != 0 ? $_REQUEST['speed']:1;
+$tempo = (isset($_REQUEST['tempo']) && (int)$_REQUEST['tempo'] != 0 ? $_REQUEST['tempo']:1;
 
 if(!preg_match('/^https?\:\/\//',$url)) {
     die('WRONG URL');
@@ -22,8 +25,8 @@ if(!preg_match('/^https?\:\/\//',$url)) {
 
 $tmpPath = tempnam('/tmp','ttsphonephp_').'.wav';
 
-$command1 = 'curl %s | '.SOX_PATH.' -t mp3 - -t wav %s vol -3dB mcompand "0.005,0.1 -47,-40,-34,-34,-17,-33" 100 "0.003,0.05 -47,-40,-34,-34,-17,-33" 400 "0.000625,0.0125 -47,-40,-34,-34,-15,-33" 1600 "0.0001,0.025 -47,-40,-34,-34,-31,-31,-0,-30" 6400 "0,0.025 -38,-31,-28,-28,-0,-25" vol 15dB highpass 22 highpass 22';
-$command1 = sprintf($command1, $url, $tmpPath);
+$command1 = 'curl %s | '.SOX_PATH.' -t mp3 - -t wav %s pitch %s speed %s tempo %s vol -3dB mcompand "0.005,0.1 -47,-40,-34,-34,-17,-33" 100 "0.003,0.05 -47,-40,-34,-34,-17,-33" 400 "0.000625,0.0125 -47,-40,-34,-34,-15,-33" 1600 "0.0001,0.025 -47,-40,-34,-34,-31,-31,-0,-30" 6400 "0,0.025 -38,-31,-28,-28,-0,-25" vol 15dB highpass 22 highpass 22';
+$command1 = sprintf($command1, $url, $pitch, $speed, $tempo, $tmpPath);
 exec($command1);
 
 $command2 = 'sox %s -p synth whitenoise vol %s | sox -m %s - -t wav - | '.LAME_PATH.' -b 72 --quiet - -';
